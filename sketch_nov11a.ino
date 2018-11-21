307fcd541d98b3f5acef355eb877f30db4d29471
Frederick Ho <fredericklcho@gmail.com>
2018-11-21 18:05:41 -0500

    code for 2 motion sensors that can detect in/out
    rather than use 4, we use 2

--- sketch_nov11a.ino
+++ sketch_nov11a.ino
@@ -1,3 +1,5 @@
+#include <QueueArray.h>
+
 /*
  * PIR sensor tester
  */
@@ -17,10 +19,16 @@ int inputPinC = 10;
 int pirStateC = LOW; 
 int valC = 0; 
 
-//int ledPinD = 11;
 int inputPinD = 9;   
 int pirStateD = LOW; 
 int valD = 0; 
+
+bool Apushed;
+bool Bpushed;
+
+QueueArray <char> abqueue;
+
+
 
 void setup() {
 //  pinMode(ledPinA, OUTPUT);      // declare LEDA as output
@@ -34,6 +42,9 @@ void setup() {
 
  // pinMode(ledPinD, OUTPUT);     // declare LEDB as output
   pinMode(inputPinD, INPUT);    //declare sensorB as input
+
+  bool Apushed = false;
+  bool Bpushed = false;
   
   Serial.begin(19200);
 }
@@ -43,104 +54,154 @@ void loop(){
   valB = digitalRead(inputPinB);  // read input value
   valC = digitalRead(inputPinC);  // read input value
   valD = digitalRead(inputPinD);  // read input value
-
    
     if (valA == HIGH) {            // check if the input is HIGH
     //digitalWrite(ledPinA, HIGH);  // turn LED ON
       if (pirStateA == LOW) {
-      // we have just turned on
-      //Serial.println("Motion (A) detected!");
-      // We only want to print on the output change, not state
-        pirStateA = HIGH;
-        }
-      }else {
-    //digitalWrite(ledPinA, LOW); // turn LED OFF
-        if (pirStateA == HIGH){
-      // we have just turned of
+          // we have just turned on
+          Serial.println("Motion (A) detected!");
+          // We only want to print on the output change, not state
+          pirStateA = HIGH;
+         //if(Apushed == false){
+            Serial.println("a push success");
+            abqueue.enqueue('a');
+           // Apushed = true;
+            //Bpushed = false;
+          //}
+      }
+    } else {
+          //digitalWrite(ledPinA, LOW); // turn LED OFF
+          if (pirStateA == HIGH){
+          // we have just turned of 
           Serial.println("Motion (A) ended!");
-      // We only want to print on the output change, not state
+          // We only want to print on the output change, not state
           pirStateA = LOW;
         }
     }
+
+     if (valB == HIGH) {            // check if the input is HIGH
+    //digitalWrite(ledPinA, HIGH);  // turn LED ON
+      if (pirStateB == LOW) {
+          // we have just turned on
+          Serial.println("Motion (B) detected!");
+          // We only want to print on the output change, not state
+          pirStateB = HIGH;
+         //if(Bpushed == false){
+            Serial.println("b push success");
+            abqueue.enqueue('b');
+            //Bpushed = true;
+            //Apushed = false;
+            //}
+          }
+        
+      } else {
+          //digitalWrite(ledPinA, LOW); // turn LED OFF
+          if (pirStateB == HIGH){
+          // we have just turned of
+          Serial.println("Motion (B) ended!");
+          // We only want to print on the output change, not state
+          pirStateB = LOW;
+        }
+    }
+
+
+    if(abqueue.count() >=2){
+      char frontitem = abqueue.front();
+      abqueue.pop();
+      char seconditem = abqueue.front();
+      Serial.println(frontitem);
+      Serial.println(seconditem);
+      abqueue.pop();
+      if(frontitem == 'b' && seconditem == 'a'){
+        Serial.println("decrement");
+      }else if (frontitem == 'a' && seconditem == 'b'){
+        Serial.println("increment");
+      }
+      while(!abqueue.isEmpty()){
+        abqueue.pop();
+      }
+    }
+    /*if(Bstatus){
+      if(Astatus){
+        Serial.println("decrement");
+        Astatus= false;
+        Bstatus = false;
+      }
+    }
     
-    if (valB == HIGH&& pirStateA == HIGH) {            // check if the input is HIGH
+    if (Astatus){
+      if (Bstatus){
+        Serial.println("increment");
+        Astatus = false;
+        Bstatus = false;
+      }
+    }*/
+    
+    
+    
+   /* if (pirStateA == HIGH)
+    {
+      if(valB == HIGH)
+          {
+          if (pirStateB == LOW) 
+              {
+              // we have just turned on
+              Serial.println("increment");
+              // We only want to print on the output change, not state
+              pirStateB = HIGH; 
+              }
+          }else 
+          {
+            if (pirStateB == HIGH){
+              // we have just turned of
+              Serial.println("Motion (Bjj) ended!");
+              // We only want to print on the output change, not state
+              pirStateB = LOW;
+            }
+          }
+    }
+    //else if (valA == HIGH){
+    //}
+    */
+    
+    
+    
+    
+  /*  if (valB == HIGH && pirStateA == HIGH) {            // check if the input is HIGH
          digitalWrite(ledPinB, HIGH);  // turn LED ON
          if (pirStateB == LOW) {
-         // we have just turned on
-         Serial.println("increment");
-         // We only want to print on the output change, not state
-         pirStateB = HIGH; 
-         }
-         } else {
-         digitalWrite(ledPinB, LOW); // turn LED OFF
-         if (pirStateB == HIGH){
-         // we have just turned of
-         Serial.println("Motion (B) ended!");
-         // We only want to print on the output change, not state
-         pirStateB = LOW;
-        }
+            // we have just turned on
+            Serial.println("increment");
+            // We only want to print on the output change, not state
+            pirStateB = HIGH; 
+            }
+   } else {
+            digitalWrite(ledPinB, LOW); // turn LED OFF
+            if (pirStateB == HIGH){
+              // we have just turned of
+              Serial.println("Motion (Bjj) ended!");
+              // We only want to print on the output change, not state
+              pirStateB = LOW;
+          }
       }
 
-
-
-
-
-   if (valC == HIGH) {            // check if the input is HIGH
-    //digitalWrite(ledPinA, HIGH);  // turn LED ON
-    if (pirStateC == LOW) {
-      // we have just turned on
-      Serial.println("C WORKS");
-      // We only want to print on the output change, not state
-      pirStateC= HIGH;
-      }}else {
-    //digitalWrite(ledPinA, LOW); // turn LED OFF
-    if (pirStateC == HIGH){
-      // we have just turned of
-      Serial.println("Motion (C) ended!");
-      // We only want to print on the output change, not state
-      pirStateC = LOW;
-    }
-  }  
-
-  if (valD == HIGH&& pirStateC == HIGH) {            // check if the input is HIGH
+    if (valA == HIGH && pirStateB == HIGH) {            // check if the input is HIGH
          //digitalWrite(ledPinB, HIGH);  // turn LED ON
-         if (pirStateD == LOW) {
-         // we have just turned on
-         Serial.println("decrement");
-         // We only want to print on the output change, not state
-         pirStateD = HIGH; 
-         }
+         if (pirStateA == LOW) {
+            // we have just turned on
+            Serial.println("deeecrement");
+            // We only want to print on the output change, not state
+            pirStateA = HIGH; 
+            }
          } else {
-         //digitalWrite(ledPinB, LOW); // turn LED OFF
-         if (pirStateD == HIGH){
-         // we have just turned of
-         //Serial.println("Motion (B) ended!");
-         // We only want to print on the output change, not state
-         pirStateD = LOW;
-        }
-      }
-
-
-
-
-   /*if (valD == HIGH) {            // check if the input is HIGH
-    //digitalWrite(ledPinA, HIGH);  // turn LED ON
-    if (pirStateD == LOW) {
-      // we have just turned on
-      Serial.println("D WORKS");
-      // We only want to print on the output change, not state
-      pirStateD = HIGH;
-      }}else {
-    //digitalWrite(ledPinA, LOW); // turn LED OFF
-    if (pirStateD == HIGH){
-      // we have just turned of
-      Serial.println("Motion (D) ended!");
-      // We only want to print on the output change, not state
-      pirStateD = LOW;
-    }
-  }*/
-
-
-
-  
+            //digitalWrite(ledPinB, LOW); // turn LED OFF
+            if (pirStateA == HIGH){
+              // we have just turned of
+              Serial.println("Motion (xx) ended!");
+              // We only want to print on the output change, not state
+              pirStateA = LOW;
+          }
+      }*/
+      
+    
 }
